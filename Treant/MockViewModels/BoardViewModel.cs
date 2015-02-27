29023f3b779ebc1b32a0757a742fbe2621d119fb
brunolm7 <brunolm7@gmail.com>
2015-02-27 18:04:25 -0300

    Mock view model for board

---
+++ Treant/MockViewModels/BoardViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treant.Domain;
+
+namespace Treant.MockViewModels
+{
+    public class BoardViewModel
+    {
+        public BoardViewModel()
+        {
+            var taskItems02 = new ObservableCollection<TaskItem>(new List<TaskItem>
+            {
+                new TaskItem { Name = "Task 02" },
+            });
+
+            var taskItems01 = new ObservableCollection<TaskItem>(new List<TaskItem>
+            {
+                new TaskItem { Name = "Task 01", TaskItems = taskItems02 },
+            });
+
+            CurrentBoard = new Board { Name = "C#" };
+
+            TaskItems = taskItems01;
+        }
+
+        public Board CurrentBoard { get; set; }
+
+        public ObservableCollection<TaskItem> TaskItems { get; private set; }
+    }
+}
