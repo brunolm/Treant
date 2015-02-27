a91e6bd9f071ecdfa8f44c41151ce365d11887a0
brunolm7 <brunolm7@gmail.com>
2015-02-27 17:42:39 -0300

    Mock View Models

---
+++ Treant/MockViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Treant.MockViewModels
+{
+    [PropertyChanged.ImplementPropertyChanged]
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            Tabs = new ObservableCollection<TabItem>(new List<TabItem>
+            {
+                new TabItem { Header = "Home", Content = "Content" },
+                new TabItem { Header = "C#", Content = "C# Content" },
+                new TabItem { Header = "WPF", Content = "WPF Content" },
+            });
+        }
+
+        public ObservableCollection<TabItem> Tabs { get; set; }
+    }
+}
